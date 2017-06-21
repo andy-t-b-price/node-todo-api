54f48332428522ecec229036e3f0cb1269ca3384
TOTEMIC\andy.price <andy.price@totemic.co.uk>
2017-06-21 09:55:57 +0100

    adding new files

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+});
+
+module.exports = {User};
